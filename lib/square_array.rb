3a482c0b57da63f0512a9ce509f4c39f8ca2b373
dependable-coder-f3d7 <tj.han@yale.edu>
2021-06-05 23:45:21 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+
+numbers = [1, 2, 3]
+square_array = [ ]
+
+def square_array(numbers)
+  while numbers.length.times do |index|
+  square_array.push(numbers[index] * numbers[index])
 end
+
+
